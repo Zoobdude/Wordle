984d838de83bdb93b538958484c9997e5c5ef37c
Zoobdude <96008479+Zoobdude@users.noreply.github.com>
2022-03-16 20:43:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import random
+from termcolor import colored
+
+allWords = open('wordlist').read().splitlines()
+theWord = random.choice(allWords)
+print(theWord)
+inWord = "0"
+
+#print(colored('hello', 'red'), colored('world', 'green'))
+
+# 1 = Not in word
+# 2 = in word not right place
+# 3 = in word right place
+
+def coolColoredWords(ltrcorrectNum, i, inWord):
+  if ltrcorrectNum == 1:
+    print(colored(inWord[i], 'red'))
+    return(colored(inWord[i], 'red'))
+  elif ltrcorrectNum == 2:
+    print(colored(inWord[i], 'yellow'))
+    return(colored(inWord[i], 'yellow'))
+  elif ltrcorrectNum == 3:
+    print(colored(inWord[i], 'green'))
+    return(colored(inWord[i], 'green'))
+  else:
+   print("somethin be broken")
+      
+while inWord != theWord:
+  inWord = input("Enter your word: ")
+  if len(inWord) == 5:
+    for i in range(5):
+      print("The current letter is", inWord[i])
+      print(f"{i} in range")
+      if inWord[i] == theWord[i]:
+       print("letter in word")
+       exec(f"ltrcorrect{i} = 3")
+      else:
+        if inWord[i] in theWord:
+          print("wrong place in word")
+          exec(f"ltrcorrect{i} = 2")
+        else:  
+          print("none in word")
+          exec(f"ltrcorrect{i} = 1")
+    print(coolColoredWords(ltrcorrect0, 0, inWord), coolColoredWords(ltrcorrect1, 1, inWord),     coolColoredWords(ltrcorrect2, 2, inWord),     coolColoredWords(ltrcorrect3, 3, inWord),     coolColoredWords(ltrcorrect4, 4, inWord) )
+  else:
+    print("thats not the right length")
+
+print(ltrcorrect0)
+print(ltrcorrect1)
+print(ltrcorrect2)
+print(ltrcorrect3)
+print(ltrcorrect4)
+#https://stackoverflow.com/questions/5036700/how-can-you-dynamically-create-variables
+#for i in range(2):
+#  exec(f"number{i} = {1}")
+#  print(number0)
+
+#https://gist.github.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b
