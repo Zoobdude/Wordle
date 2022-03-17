558ae99317a3623ead49fa39a00a9979ad7aebe6
Zoobdude <96008479+Zoobdude@users.noreply.github.com>
2022-03-17 08:14:03 +0000

    Added comments

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ inWord = "0"
 
 print(colored(text2art("Wordle"), 'green'))
 
-
+#-----------------------------------------------------------
+# Converts numbers to the coloured letters:
 # 1 = Not in word
 # 2 = in word not right place
 # 3 = in word right place
@@ -23,11 +24,16 @@ def coolColoredWords(ltrcorrectNum, i, inWord):
     return(colored(inWord[i], 'green'))
   else:
    print("somethin be broken")
-      
+
+#----------------------------------------------------------
+
 while inWord != theWord:
+  #guess input
   inWord = input("Enter your word: ")
   if len(inWord) == 5:
+    #checks guess length
     for i in range(5):
+      #checks which letters are correct and gives them numbers
       if inWord[i] == theWord[i]:
        exec(f"ltrcorrect{i} = 3")
       else:
@@ -35,6 +41,7 @@ while inWord != theWord:
           exec(f"ltrcorrect{i} = 2")
         else:  
           exec(f"ltrcorrect{i} = 1")
+    #calls subroutine to covert numbers to coloured leters
     print(coolColoredWords(ltrcorrect0, 0, inWord), coolColoredWords(ltrcorrect1, 1, inWord),     coolColoredWords(ltrcorrect2, 2, inWord),     coolColoredWords(ltrcorrect3, 3, inWord),     coolColoredWords(ltrcorrect4, 4, inWord) )
   else:
     print("thats not the right length")
