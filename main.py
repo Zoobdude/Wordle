8570a18f0b28c574a5565a3ab34b3d1edfb3300b
Zoobdude <96008479+Zoobdude@users.noreply.github.com>
2022-03-18 13:13:53 +0000

    Made it look more beautiful. thanks fwinter

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import random
 from termcolor import colored
 from art import *
+import os
 
 
 allWords = open('wordlist').read().splitlines()
 theWord = random.choice(allWords)
 inWord = "0"
+allText = []
 
-print(colored(text2art("Wordle"), 'green'))
+def allTextPrint(inputStuff):
+  global allText
+  allText.append(inputStuff)
+def allTextOutput():
+  os.system('clear')
+  for i in allText:
+    for x in i:
+      print(x,end='')
+
+allTextPrint(colored(text2art("Wordle"), 'green'))
+allTextOutput()
 
 #-----------------------------------------------------------
 # Converts numbers to the coloured letters:
@@ -29,7 +41,7 @@ def coolColoredWords(ltrcorrectNum, i, inWord):
 
 while inWord != theWord:
   #guess input
-  inWord = input("Enter your word: ")
+  inWord = input("\nEnter your word: ")
   if len(inWord) == 5:
     #checks guess length
     for i in range(5):
@@ -42,6 +54,9 @@ while inWord != theWord:
         else:  
           exec(f"ltrcorrect{i} = 1")
     #calls subroutine to covert numbers to coloured leters
-    print(coolColoredWords(ltrcorrect0, 0, inWord), coolColoredWords(ltrcorrect1, 1, inWord),     coolColoredWords(ltrcorrect2, 2, inWord),     coolColoredWords(ltrcorrect3, 3, inWord),     coolColoredWords(ltrcorrect4, 4, inWord) )
+    
+    allTextPrint([coolColoredWords(ltrcorrect0, 0, inWord), coolColoredWords(ltrcorrect1, 1, inWord),     coolColoredWords(ltrcorrect2, 2, inWord),     coolColoredWords(ltrcorrect3, 3, inWord),     coolColoredWords(ltrcorrect4, 4, inWord),'\n'])
+    allTextOutput()
+    
   else:
     print("thats not the right length")
