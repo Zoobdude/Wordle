1e47a7e5278da19dab78d77a401f6f58e4bcd769
Zoobdude <96008479+Zoobdude@users.noreply.github.com>
2022-03-21 14:18:58 +0000

    Added high score

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import os
 from replit import db
 import time
 
+highScoreTime = db["hS"]
 #-----------------------------------------------------
 #Picks random word from file
 allWords = open('wordlist').read().splitlines()
-theWord = random.choice(allWords)
+theWord = "penis" #random.choice(allWords)
 #-----------------------------------------------------
 #Dels extra rubbish on variable
 allWords = ""
@@ -29,7 +30,7 @@ def allTextOutput():
       print(x,end='')   
 #-----------------------------------------------------
 #timer
-start_time = time.time()
+startTime = time.time()
 #input("Press Enter to stop")
 #end_time = time.time()
 #time_lapsed = end_time - start_time
@@ -84,6 +85,12 @@ while inWord != theWord:
     print("thats not the right length")
     time.sleep(1)
     allTextOutput()
-end_time = time.time()
-time_lapsed = end_time - start_time
-print(time_lapsed)
+endTime = time.time()
+timeLapsed = endTime - startTime
+print("You did it in", str(time_lapsed) +"s")
+
+if highScoreTime > timeLapsed:
+  db["hS"] = timeLapsed
+  print("New quickest time")
+else:
+  print("The fastest time was", highScoreTime)
